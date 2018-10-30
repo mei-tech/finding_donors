1b1018e5d9b5833b3a860f7c31e459e0d7f2ba1f
Andreas Meichelboeck <andreas.meichelboeck@gmail.com>
2018-10-30 17:17:13 +0100

    first final release

--- config/config.sh
+++ config/config.sh
@@ -3,26 +3,26 @@
 # install default requirements
 cd /home/tf/config
 if [[ $(wc -l < "default_requirements.txt") -ge 0 ]]; then
-  pip install -r default_requirements.txt
+  pip3 install -r default_requirements.txt
 fi
 
 # jupyter notebooks config
 ## jupyter themes
-pip install jupyterthemes
+pip3 install jupyterthemes
 ### select dark theme
 jt -t chesterish
 ## extensions
-pip install jupyter_contrib_nbextensions
+pip3 install jupyter_contrib_nbextensions
 # jupyter nbextension enable
 
 ## configurator
-pip install jupyter_nbextensions_configurator
+pip3 install jupyter_nbextensions_configurator
 jupyter nbextensions_configurator enable --user
 
 cd /notebooks
 # install project requirements
 if [[ $(wc -l < "requirements.txt") -ge 0 ]]; then
-  pip install -r requirements.txt
+  pip3 install -r requirements.txt
 fi
 
 /run_jupyter.sh --allow-root
