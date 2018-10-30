11e694f52f5c289e9105829569460e7c18fad34f
Andreas Meichelboeck <andreas.meichelboeck@gmail.com>
2018-10-30 15:21:13 +0100

    changed to pip/py2 and changed docker image and container name to an unique one

--- config/config.sh
+++ config/config.sh
@@ -3,16 +3,26 @@
 # install default requirements
 cd /home/tf/config
 if [[ $(wc -l < "default_requirements.txt") -ge 0 ]]; then
-  pip3 install -r default_requirements.txt
+  pip install -r default_requirements.txt
 fi
 
-# ipython notebooks config
-# TODO
+# jupyter notebooks config
+## jupyter themes
+pip install jupyterthemes
+### select dark theme
+jt -t chesterish
+## extensions
+pip install jupyter_contrib_nbextensions
+# jupyter nbextension enable
+
+## configurator
+pip install jupyter_nbextensions_configurator
+jupyter nbextensions_configurator enable --user
 
 cd /notebooks
 # install project requirements
 if [[ $(wc -l < "requirements.txt") -ge 0 ]]; then
-  pip3 install -r requirements.txt
+  pip install -r requirements.txt
 fi
 
 /run_jupyter.sh --allow-root
